6e658ebc1adfd3e36f472bbf7db97f9af1c0cf18
kazirobinking <kazirobinking@gmail.com>
2026-02-11 23:01:50 +0600

    updated

--- src/data/products.js
+++ src/data/products.js
@@ -1,12 +1,12 @@
 export const products = [
-  { id: 1, name: "Chocolate Cake", price: 15, ingredients: "Flour, Cocoa, Sugar" },
-  { id: 2, name: "Vanilla Cake", price: 12, ingredients: "Flour, Vanilla, Sugar" },
-  { id: 3, name: "Croissant", price: 5, ingredients: "Butter, Flour" },
-  { id: 4, name: "Donut", price: 3, ingredients: "Flour, Sugar, Oil" },
-  { id: 5, name: "Muffin", price: 4, ingredients: "Egg, Flour, Milk" },
-  { id: 6, name: "Brownie", price: 6, ingredients: "Chocolate, Butter" },
-  { id: 7, name: "Cupcake", price: 4, ingredients: "Cream, Flour" },
-  { id: 8, name: "Bagel", price: 3, ingredients: "Flour, Yeast" },
-  { id: 9, name: "Apple Pie", price: 10, ingredients: "Apple, Flour, Sugar" },
-  { id: 10, name: "Cheesecake", price: 18, ingredients: "Cheese, Sugar" }
+  { id: 1, name: "Chocolate Cake", price: 15, ingredients: "Flour, Cocoa, Sugar", image: "https://bluebowlrecipes.com/wp-content/uploads/2023/08/chocolate-truffle-cake-8844.jpg" },
+  { id: 2, name: "Vanilla Cake", price: 12, ingredients: "Flour, Vanilla, Sugar", image: "https://thestayathomechef.com/wp-content/uploads/2022/09/Most-Amazing-Vanilla-Cake-1.jpg" },
+  { id: 3, name: "Croissant", price: 5, ingredients: "Butter, Flour", image: "https://www.diplomacyandcommerce.rs/wp-content/uploads/2021/01/French-Croissants-SM-2363.jpg" },
+  { id: 4, name: "Donut", price: 3, ingredients: "Flour, Sugar, Oil", image: "https://static.vecteezy.com/system/resources/previews/026/569/799/original/colorful-and-tasty-various-donuts-with-colorful-sprinkles-sugar-delicious-dessert-donuts-background-concept-by-ai-generated-free-photo.jpg" },
+  { id: 5, name: "Muffin", price: 4, ingredients: "Egg, Flour, Milk", image: "https://natashaskitchen.com/wp-content/uploads/2019/02/Breakfast-Muffins-5.jpg" },
+  { id: 6, name: "Brownie", price: 6, ingredients: "Chocolate, Butter", image: "https://lilluna.com/wp-content/uploads/2017/11/fudgy-brownies-resize-15.jpg" },
+  { id: 7, name: "Cupcake", price: 4, ingredients: "Cream, Flour", image: "https://4.bp.blogspot.com/-TgxnxZy2EjY/TkiN1LyiTmI/AAAAAAAACKk/I_8zxxVPKPc/s1600/IMG_6155csqs.jpg" },
+  { id: 8, name: "Bagel", price: 3, ingredients: "Flour, Yeast", image: "https://static.vecteezy.com/system/resources/previews/044/419/833/non_2x/golden-brown-bagels-with-sesame-seeds-stacked-on-a-wooden-board-photo.jpeg" },
+  { id: 9, name: "Apple Pie", price: 10, ingredients: "Apple, Flour, Sugar", image: "https://i1.wp.com/www.powered-by-mom.com/wp-content/uploads/2017/07/apple-pie-portraitr.jpg?resize=950%2C1425" },
+  { id: 10, name: "Cheesecake", price: 18, ingredients: "Cheese, Sugar", image: "https://www.tasteofhome.com/wp-content/uploads/2018/01/Triple-Berry-No-Bake-Cheesecake_EXPS_TOHcom19_138951_B01_30_6b-3.jpg" }
 ];
